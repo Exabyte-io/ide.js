e8d60aa03b77c40a193459d82c33ca57c42aa217
tjduigna <thomas.j.duignan@gmail.com>
2022-07-28 12:41:37 -0400

    feat: package installs and test runs

---
+++ tests/compute.test.js
@@ -0,0 +1,18 @@
+import { expect } from "chai";
+import { mix } from "mixwith";
+
+import { ComputedEntityMixin } from "../src/compute";
+
+class Base {}
+
+class Computer extends mix(Base).with(ComputedEntityMixin) {}
+
+describe("Model", () => {
+    const obj = {};
+
+    it("can be created", () => {
+        const app = new Computer(obj);
+        const config = app.constructor.getDefaultComputeConfig();
+        expect(config.ppn).to.equal(1);
+    });
+});
