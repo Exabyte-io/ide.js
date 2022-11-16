d1d16b7664e3cd9de347efb2279a6c8604870722
Yurii Vlasiuk <yuriivlasiuk@gmail.com>
2022-11-16 14:28:33 +0200

    fix: eslint issues in src and tests

--- src/enums.js
+++ src/enums.js
@@ -1,48 +1,58 @@
 export const EMAIL_NOTIFICATION_OPTIONS_PBS = [
     {
-        label: 'never',
-        value: 'n'
+        label: "never",
+        value: "n",
     },
     {
-        label: 'abort',
-        value: 'a'
+        label: "abort",
+        value: "a",
     },
     {
-        label: 'begin',
-        value: 'b'
+        label: "begin",
+        value: "b",
     },
     {
-        label: 'end',
-        value: 'e'
-    }
+        label: "end",
+        value: "e",
+    },
 ];
 
 // TODO: adjust to make modular to work not only with PBS, but SLURM etc.
 class RMSNotificationsHandler {
-
     constructor(type) {
         this.type = type;
     }
 
     get options() {
         const config = {
-            "PBS": EMAIL_NOTIFICATION_OPTIONS_PBS,
+            PBS: EMAIL_NOTIFICATION_OPTIONS_PBS,
         };
         return config[this.type] || EMAIL_NOTIFICATION_OPTIONS_PBS;
     }
 
-    _getOptionValueByLabel(label) {return (this.options.find(o => o.label === label) || {}).value}
+    _getOptionValueByLabel(label) {
+        return (this.options.find((o) => o.label === label) || {}).value;
+    }
 
-    get never() {return this._getOptionValueByLabel("never")}
+    get never() {
+        return this._getOptionValueByLabel("never");
+    }
 
-    get abort() {return this._getOptionValueByLabel("abort")}
+    get abort() {
+        return this._getOptionValueByLabel("abort");
+    }
 
-    get begin() {return this._getOptionValueByLabel("begin")}
+    get begin() {
+        return this._getOptionValueByLabel("begin");
+    }
 
-    get end() {return this._getOptionValueByLabel("end")}
+    get end() {
+        return this._getOptionValueByLabel("end");
+    }
 
-    get abe() {return this.abort + this.begin + this.end}
-
+    get abe() {
+        return this.abort + this.begin + this.end;
+    }
 }
 
 const handler = new RMSNotificationsHandler("PBS");
