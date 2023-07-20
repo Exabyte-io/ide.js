8b5ffd49903626202f9702660983a0e9833aa3e3
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2023-07-20 12:44:57 +0300

    chore: add tests for getApproximateCharge

--- tests/compute.test.js
+++ tests/compute.test.js
@@ -1,12 +1,23 @@
 /* eslint-disable max-classes-per-file */
+import { InMemoryEntity } from "@exabyte-io/code.js/dist/entity";
 import { expect } from "chai";
 import { mix } from "mixwith";
 
 import { ComputedEntityMixin } from "../src/compute";
 
-class Base {}
+class Computer extends mix(InMemoryEntity).with(ComputedEntityMixin) {}
 
-class Computer extends mix(Base).with(ComputedEntityMixin) {}
+function assertApproximateCharge(compute, expectedCharge) {
+    const settings = { baseChargeRate: 1 };
+    const queueMultipliers = {
+        D: 2,
+        OR: 1,
+    };
+
+    const app = new Computer({ compute });
+    const charge = app.getApproximateCharge(settings, queueMultipliers);
+    expect(charge).to.equal(expectedCharge);
+}
 
 describe("Model", () => {
     const obj = {};
@@ -15,5 +26,11 @@ describe("Model", () => {
         const app = new Computer(obj);
         const config = app.constructor.getDefaultComputeConfig();
         expect(config.ppn).to.equal(1);
+    });
+
+    it("calculates approximate charge", () => {
+        assertApproximateCharge({ queue: "D", timeLimit: "01:00:00" }, 2);
+        assertApproximateCharge({ queue: "D", timeLimit: "70:00:00" }, 140);
+        assertApproximateCharge({ queue: "OR", timeLimit: "70:00:00" }, 70);
     });
 });
